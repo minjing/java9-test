5dd970517a22194e34285fd88c08234b8d61485e
minjing <min_jing@aliyun.com>
2017-10-20 16:10:11 +0800

    Add easytext module and add idea project file

--- build-helloworld.sh
+++ build-helloworld.sh
@@ -1,10 +1,10 @@
-javac -d out/helloworld src/helloworld/com/javamodularity/helloworld/HelloWorld.java src/helloworld/module-info.java
+mkdir out/helloworld
+javac -d out/helloworld helloworld/src/com/javamodularity/helloworld/HelloWorld.java helloworld/src/module-info.java
 
 mkdir mods
 jar -cfe mods/helloworld.jar com.javamodularity.helloworld.HelloWorld -C out/helloworld .
 
 java --module-path out --module helloworld/com.javamodularity.helloworld.HelloWorld
-
 java --show-module-resolution --limit-modules java.base --module-path mods --module helloworld
 
 rm -rf image/helloworld
